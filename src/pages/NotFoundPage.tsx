68e2640c2f8b6e1fbd3f445516aaa2ab8e410aba
Kevin B <papershack@gmail.com>
2024-03-01 21:43:54 -0800

    fix not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-import { Stack } from "@mui/system";
+import { Box, Stack } from "@mui/system";
 import ArrowLeft from "@mui/icons-material/WestOutlined";
+import { ROUTES } from "routes";
 
 export const NotFoundPage = (): JSX.Element => {
   return (
     <Stack gap="20px">
       <h2>Not Found</h2>
       <span>Sorry, the page you are looking for does not exist.</span>
-      <Link to=".."><ArrowLeft /> Go Back</Link>
+        <Box display="flex" alignItems="center" gap="10px" component={Link} to={ROUTES.dashboard.path}>
+          <ArrowLeft fontSize="small" /> Go to Dashboard
+        </Box>
     </Stack>
   );
 };
