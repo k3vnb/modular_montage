89f43355d4ddd0737f8858f909f37b7123def80a
Kevin B <boyle.kevin.michael@gmail.com>
2024-05-16 21:00:02 -0700

    simplify typography story

--- src/theme/ThemeTypography.stories.tsx
+++ src/theme/ThemeTypography.stories.tsx
@@ -1,39 +1,26 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Stack, Box } from '@mui/system';
 import { THEME_FONTS } from './typography';
+import { UnstyledList, ListItem } from 'global/components/List';
 
-const formatTitle = (title: string) => {
+function formatTitle (title: string) {
   const [type, size, variation] = title
     .split(/(?=[A-Z])/)
     .map((word) => word[0].toUpperCase() + word.slice(1));
   
   return [[type, size].join(' '), variation].filter(Boolean).join(' / ');
+}
 
-};
-
-const Divider = () => (
-  <Box
-    sx={{
-      width: '100%',
-      height: '1px',
-      minWidth: '20px',
-      backgroundColor: theme => theme.palette.neutral[50],
-    }}
-  />
-);
-
-const TypographyExamples: React.FC = () => {
+const TypographyExamples = (): JSX.Element => {
   return (
-    <Stack gap="30px" divider={<Divider />}>
+    <UnstyledList>
       {Object.entries(THEME_FONTS).map(([key, fontStyles]) => {
         return (
-          <Box key={key} sx={{ ...fontStyles }}>
+          <ListItem key={key} sx={{ ...fontStyles, marginY: '15px' }}>
             {formatTitle(key)}
-          </Box>
+          </ListItem>
         );
       })}
-    </Stack>
+    </UnstyledList>
   );
 };
 
